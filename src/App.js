e25a299cd3a8a591853fd01d51e4dbb4b21a0fa3
Danny Meyer <Danny.Meyer@gmail.com>
2018-11-26 02:33:59 -0500

    styled components

--- src/App.js
+++ src/App.js
@@ -1,33 +1,10 @@
 import 'rc-slider/assets/index.css';
 import React, {Component} from 'react';
 import Slider from 'rc-slider';
+import styled from 'styled-components';
 
 const Range = Slider.Range;
 
-const style = {
-  sliderContainer: {
-    margin: 50,
-  },
-  inputContainer: {
-    margin: "10px",
-  },
-  newButton: {
-    backgroundColor: "bisque",
-    width: "100px",
-    padding: "20px",
-    textAlign: "center",
-    margin: 'auto 10px'
-  },
-  deleteButton: {
-    backgroundColor: "bisque",
-    width: "50px",
-    padding: "10px",
-    textAlign: "center",
-    margin: 'auto 0',
-    marginTop: '40px'
-  },
-  sliderLabelContainer: {display: 'flex', flexDirection: 'column'},
-};
 
 class App extends Component {
   constructor(props) {
@@ -74,24 +51,24 @@ class App extends Component {
 
   renderSlider = (slider, i) => {
     return (
-      <div key={i} style={style.sliderContainer}>
+      <SliderContainer key={i} >
         <div style={style.sliderLabelContainer}>
-          <h3 style={{alignSelf: 'center'}}>{this.state.sliders[i].title_label}</h3>
-          <div style={{display: 'flex', justifyContent: 'space-between', flexDirection: 'row'}}>
+          <SliderTitle>{this.state.sliders[i].title_label}</SliderTitle>
+          <SliderLabelContainer>
             <p>{this.state.sliders[i].left_label}</p>
             <p>{this.state.sliders[i].right_label}</p>
-          </div>
+          </SliderLabelContainer>
         </div>
         <Range
           value={this.state.sliders[i].value}
           onChange={(e) => this.handleChange(e, i)}
         />
-        <div style = {{display: "flex", justifyContent: "flex-end", flexDirection: 'row'}}>
-          <div onClick={() => this.removeSlider(i)}>
-           <p style={style.deleteButton}>X</p>
-          </div>
-        </div>
-      </div>
+        <DeleteButtonContainer>
+          <DeleteButton onClick={() => this.removeSlider(i)}>
+           <p>X</p>
+          </DeleteButton>
+        </DeleteButtonContainer>
+      </SliderContainer>
     );
   };
 
@@ -164,10 +141,10 @@ class App extends Component {
 
     if (total > 0) {
       return (
-        <div style={{display: 'flex', flexDirection: 'column'}}>
-          <p style={{alignSelf: 'center'}}>{score} / {total}</p>
-          <p style={{alignSelf: 'center'}}>Score: {(score / total).toFixed(3)}</p>
-        </div>
+        <ScoreContainer>
+          <Score>{score} / {total}</Score>
+          <Score>Score: {(score / total).toFixed(3)}</Score>
+        </ScoreContainer>
       )
     } else {
       return <div />
@@ -176,33 +153,112 @@ class App extends Component {
 
   render() {
     return (
-      <div style={{backgroundColor: 'whitesmoke', height: "100%"}}>
-        <div style={{display: "flex", flexDirection: "row"}}>
-          <div
-            onClick={() => this.newSlider()}
-            style={style.newButton}>
-            New
-          </div>
-          <div style={style.inputContainer}>
-            <p>Title</p>
-            <input onChange={(e) => this.titleChange(e)} value={this.state.new.title}/>
-          </div>
-          <div style={style.inputContainer}>
-            <p>Left Label</p>
-            <input onChange={(e) => this.leftChange(e)} value={this.state.new.left}/>
-          </div>
-          <div style={style.inputContainer}>
-            <p>Right Label</p>
-            <input onChange={(e) => this.rightChange(e)}  value={this.state.new.right}/>
-          </div>
+      <MainContainer>
+        <NewContainer>
+          <NewButton
+            onClick={() => this.newSlider()}>
+            <p>New</p>
+          </NewButton>
 
-        </div>
+          <InputContainer>
+            <p>Title: </p>
+            <Input onChange={(e) => this.titleChange(e)} value={this.state.new.title}/>
+          </InputContainer>
+
+          <InputContainer>
+          <p>Left: </p>
+            <Input onChange={(e) => this.leftChange(e)} value={this.state.new.left}/>
+          </InputContainer>
+
+          <InputContainer>
+            <p>Right:</p>
+            <Input onChange={(e) => this.rightChange(e)}  value={this.state.new.right}/>
+          </InputContainer>
+        </NewContainer>
         {this.renderScore()}
         {this.renderSliders()}
-      </div>
+      </MainContainer>
     );
   }
 }
+
+const Input = styled.input`
+    height: 30px;
+    font-size: 20px;
+`;
+
+const Score = styled.h3`
+  align-self: center;
+  margin: 0;
+  margin-top: 10px;
+`;
+
+const ScoreContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const DeleteButtonContainer = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  flex-direction: row;
+`;
+
+const SliderLabelContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  flex-direction: row;
+`;
+
+const NewContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+
+const MainContainer = styled.div`
+  background-color: whitesmoke;
+  min-height: 100vh;
+  margin: 0;
+  padding: 0;
+`;
+
+const SliderTitle = styled.h3`
+  align-self: center
+`;
+
+const SliderContainer = styled.div`
+  margin: 5px 50px 5px 50px;
+  
+`;
+
+const InputContainer = styled.div`
+  margin: 10px
+`;
+
+const NewButton = styled.div`
+    background-color: bisque;
+    width: 100px;
+    padding: 20px;
+    text-align: center;
+    margin: auto 0;
+`;
+
+const DeleteButton = styled.div`
+    background-color: bisque;
+    width: 50px;
+    padding: 10px;
+    text-align: center;
+    margin: auto 0;
+    margin-top: 30px;
+`;
+
+const style = {
+  deleteButton: {
+
+  },
+  sliderLabelContainer: {display: 'flex', flexDirection: 'column'},
+};
+
 
 export default App
 
