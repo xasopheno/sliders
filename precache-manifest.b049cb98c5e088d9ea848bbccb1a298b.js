ef3712aac3e0e0ce89620fbc25b3d4ecd1726f44
Danny Meyer <Danny.Meyer@gmail.com>
2018-11-26 14:25:55 -0500

    Updates

---
+++ precache-manifest.b049cb98c5e088d9ea848bbccb1a298b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7834741cb41312add591",
+    "url": "/sliders/static/css/main.7df5981a.chunk.css"
+  },
+  {
+    "revision": "7834741cb41312add591",
+    "url": "/sliders/static/js/main.7834741c.chunk.js"
+  },
+  {
+    "revision": "770142294ee78bc6ca1e",
+    "url": "/sliders/static/css/1.b72d5083.chunk.css"
+  },
+  {
+    "revision": "770142294ee78bc6ca1e",
+    "url": "/sliders/static/js/1.77014229.chunk.js"
+  },
+  {
+    "revision": "c060d3513c040f14c1eb",
+    "url": "/sliders/static/js/runtime~main.c060d351.js"
+  },
+  {
+    "revision": "5b4799bca4ad3d3531b8c4357d1c65e2",
+    "url": "/sliders/index.html"
+  }
+];
